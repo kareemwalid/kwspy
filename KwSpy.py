99f212883293112552388b7a6886e7c8f2a53ddf
kareem walid <kareemwalid063@gmail.com>
2017-09-21 04:31:57 +0200

    KwSpy Sniffing Tools

---
+++ KwSpy.py
@@ -0,0 +1,112 @@
+import os
+from time import sleep
+banner="""
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<kareem Walid>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<KwInfoSec!!!>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+Run Your antivirus and wish you to be in safe ^^
+c0der Is Here
+"""
+print(banner)
+def linux():
+	sleep(6)
+	print("""
+	[1]-Xerosploit\t\t[7]-Sniffy
+	[2]-Srut3k1t\t\t[8]-isniff for iphone 
+	[3]-Simple sniff
+	[4]-Sniff Room
+	[5]-Sniffer
+	[6]-SniffLaB
+	""")
+	use=str(input("Enter > "))
+	sleep(3)
+	if use == '1':
+		xer="git clone https://github.com/LionSec/xerosploit.git"
+		print("okay :D wait some seconds while download it !!")
+		os.system(xer)
+		sleep(16)
+		print("please Wait")
+		sleep(3)
+		print("c0der Was Here =========KwInfoSec=========")
+		sleep(3)
+		print("finish BYE BYE ")
+		exit()
+	elif use == '2':
+			print("okay :D wait some seconds while downloading it !!")
+			bru="git clone https://github.com/ex0dus-0x/brut3k1t.git"
+			os.system(bru)
+			sleep(12)
+			print("please wait.....")
+			sleep(7)
+			print("c0der Was Here =========KwInfoSec=========")
+			sleep(3)
+			print("finish BYE BYE ")
+			exit()
+	elif use == '3':
+		print("okay :D wait some seconds while downloading it !!")
+		sni="git clone https://github.com/lukapusic/sniff.git"
+		os.system(sni)
+		sleep(12)
+		print("please wait.....")
+		sleep(7)
+		print("c0der Was Here =========KwInfoSec=========")
+		sleep(3)
+		print("finish BYE BYE ")
+		exit()
+	elif use == '4':
+		print("okay :D wait some seconds while downloading it !!")
+		sroom="git clone https://github.com/alainiamburg/sniffROM.git"
+		os.system(sroom)
+		sleep(12)
+		print("please wait.....")
+		sleep(7)
+		print("c0der Was Here =========KwInfoSec=========")
+		sleep(3)
+		print("finish BYE BYE ")
+		exit()
+	elif use == '5':
+		print("okay :D wait some seconds while downloading it !!")
+		snifer="git clone https://github.com/jeffh/sniffer.git"
+		os.system(snifer)
+		sleep(12)
+		print("please wait.....")
+		sleep(7)
+		print("c0der Was Here =========KwInfoSec=========")
+		sleep(3)
+		print("finish BYE BYE ")
+		exit()
+	elif use == '6':
+		print("okay :D wait some seconds while downloading it !!")
+		lab="git clone https://github.com/andrewhilts/snifflab.git"
+		os.system(lab)
+		sleep(12)
+		print("please wait.....")
+		sleep(7)
+		print("c0der Was Here =========KwInfoSec=========")
+		sleep(3)
+		print("finish BYE BYE ")
+		exit()
+	elif use == '7':
+		print("okay :D wait some seconds while downloading it !!")
+		sniffy="git clone https://github.com/OffensivePython/Sniffy.git"
+		os.system(sniffy)
+		sleep(12)
+		print("please wait.....")
+		sleep(7)
+		print("c0der Was Here =========KwInfoSec=========")
+		sleep(3)
+		print("finish BYE BYE ")
+		exit()
+	elif use == '8':
+		print("okay :D wait some seconds while downloading it !!")
+		iphone="git clone https://github.com/hubert3/iSniff.git"
+		os.system(iphone)
+		sleep(12)
+		print("please wait.....")
+		sleep(7)
+		print("c0der Was Here =========KwInfoSec=========")
+		sleep(3)
+		print("finish BYE BYE ")
+		exit()
+	else:
+		exit()
+linux()
